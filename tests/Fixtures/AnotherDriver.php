3577cbcabc64c39589d413fd924a4633c813e791
YusufIKhwan <91899067+YusufIKhwan@users.noreply.github.com>
2022-04-20 13:06:57 +0800

    livechat

---
+++ tests/Fixtures/AnotherDriver.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace BotMan\BotMan\Tests\Fixtures;
+
+class AnotherDriver extends TestDriver
+{
+    /**
+     * Return the driver name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'Another';
+    }
